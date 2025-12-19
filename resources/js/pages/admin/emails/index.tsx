a259d67500c69a370256b031e560bef862e71bb7
macevedoPhonix <macevedo@phonix.network>
2025-12-19 18:00:18 -0500

    Enhance layout by setting minimum height for email campaign page

--- resources/js/pages/admin/emails/index.tsx
+++ resources/js/pages/admin/emails/index.tsx
@@ -97,7 +97,7 @@ const SendEmailCampaign: React.FC = () => {
     }, [flash?.success]);
 
     return (
-        <div className="bg-gray-50">
+        <div className="min-h-screen bg-gray-50">
             <Header />
             <GoDashboard />
             <div className="mx-auto max-w-6xl">
