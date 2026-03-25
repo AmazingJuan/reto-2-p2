48052b6bc33390688ae1ef271c14eff63ddf386a
xDiegoAGG <dagonzal11@eafit.edu.co>
2026-03-25 16:19:36 -0500

    feat: add Docker setup and configure for deployment

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+FROM php:8.4-apache
+
+ENV APACHE_DOCUMENT_ROOT /var/www/html/public
+
+RUN apt-get update && apt-get install -y \
+    git curl zip unzip libpng-dev libonig-dev libxml2-dev
+
+RUN docker-php-ext-install pdo pdo_mysql
+
+COPY --from=composer:latest /usr/bin/composer /usr/bin/composer
+
+RUN a2enmod rewrite \
+    && sed -ri -e 's!/var/www/html!${APACHE_DOCUMENT_ROOT}!g' /etc/apache2/sites-available/*.conf \
+    && sed -ri -e 's!/var/www/!${APACHE_DOCUMENT_ROOT}!g' /etc/apache2/apache2.conf /etc/apache2/conf-available/*.conf
+
+WORKDIR /var/www/html
+
+COPY . .
+
+RUN composer install
+
+RUN chmod -R 777 storage bootstrap/cache
+
+EXPOSE 80
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,21 +14,17 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         User::create([
-            'name' => 'Usuario de Prueba',
-            'email' => 'test@example.com',
-            'password' => Hash::make('1234'),
-            'phone'=> '1234567890',
+            'name' => 'Christian Correa',
+            'email' => 'profesional-INN@trainingcorporation.com.co',
+            'password' => Hash::make('profesionalINN'),
+            'phone'=> '3107288219',
         ]);
 
         User::create([
-            'name' => 'Diego',
-            'email' => 'diego@example.com',
-            'password' => Hash::make('diegogonzalez'),
-            'phone' => '0987654321',
+            'name' => 'Eafit',
+            'email' => 'reto2@eafit.edu.co',
+            'password' => Hash::make('reto2p2*'),
+            'phone' => '123456789',
         ]);
-
-        // Datos de ejemplo porque me dió mucha pereza crearlos manualmente
-    
-        $this->call(ExampleDataSeeder::class);
     }
 }
