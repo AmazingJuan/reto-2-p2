9fbc2c8d83f843889800d81590cc2d81eea32148
Juan Avendaño <157389739+AmazingJuan@users.noreply.github.com>
2026-06-09 20:09:14 -0500

    Fix/client sees quotation (#120)
    * now quotation seen status is not updated everytime the client open the link. Instead it only does a database transcation when client hasn't seen the link
    
    * fixed

--- app/Http/Controllers/Quotation/QuotationController.php
+++ app/Http/Controllers/Quotation/QuotationController.php
@@ -126,18 +126,18 @@ class QuotationController extends Controller
 
     public function receiveQuotation(Request $request, string $quotationId): RedirectResponse
     {
-        $quotation = QuotationProposalOrder::findOrFail($quotationId);
+        $quotation = QuotationProposalOrder::find('id', $quotationId);
+
+        if (! $quotation) {
+            return redirect()->route('home')->with('error', 'No se encontró la cotización.');
+        }
 
         if (! $request->hasValidSignature()) {
-            return redirect()->route('home', [
-                'error' => 'La URL de la cotización ha expirado o no es válida.',
-            ]);
+            return redirect()->route('home')->with('error', 'La URL de la cotización ha expirado o no es válida.');
         }
 
         if (! $quotation->getQuotationUrl()) {
-            return redirect()->route('home', [
-                'error' => 'La cotización aún no tiene un documento disponible.',
-            ]);
+            return redirect()->route('home')->with('error', 'La cotización aún no tiene un documento disponible.');
         }
 
         if (! $quotation->getViewedByClient()) {
